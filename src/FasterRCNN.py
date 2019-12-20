a88f282799303712a7a160a78a2a9b5a4289e4e2
Daniel Zeiberg <zeiberg.d@husky.neu.edu>
2019-12-20 17:28:24 -0500

    switch model

--- src/FasterRCNN.py
+++ src/FasterRCNN.py
@@ -6,21 +6,22 @@ from torchvision.models.detection.rpn import AnchorGenerator
 import torch
 
 def get_model_instance_segmentation_unmodified(num_classes):
-	# load an instance segmentation model pre-trained pre-trained on COCO
-	model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+	# # load an instance segmentation model pre-trained pre-trained on COCO
+	# model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
 
-	# get number of input features for the classifier
-	in_features = model.roi_heads.box_predictor.cls_score.in_features
-	# replace the pre-trained head with a new one
-	model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+	# # get number of input features for the classifier
+	# in_features = model.roi_heads.box_predictor.cls_score.in_features
+	# # replace the pre-trained head with a new one
+	# model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-	# now get the number of input features for the mask classifier
-	in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-	hidden_layer = 256
-	# and replace the mask predictor with a new one
-	model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-													   hidden_layer,
-													   num_classes)
+	# # now get the number of input features for the mask classifier
+	# in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+	# hidden_layer = 256
+	# # and replace the mask predictor with a new one
+	# model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+	# 												   hidden_layer,
+	# 												   num_classes)
+	model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, num_classes=num_classes)
 
 	return model
 
